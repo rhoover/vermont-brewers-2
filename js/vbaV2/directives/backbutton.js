a40a0948fa631b4fd55e84fe1003aede91a4d1e8
robinHoover <robin.hoover@gmail.com>
2014-05-09 20:42:20 -0400

    Link Function For Behavior

--- js/vbaV2/directives/backbutton.js
+++ js/vbaV2/directives/backbutton.js
@@ -4,14 +4,12 @@ angular.module('vbaV2App')
   .directive('backButton', [function () {
     return {
         restrict: 'A',
-        compile: function (tElement, tAttributes, transclude) {
-            return function (scope, element, attrs) {
-                element.bind('click', goBack);
-                function goBack() {
-                    history.back();
-                    scope.$apply();
-                };
-            } //end return function
-        } //end compile
+        link: function (scope, element, attrs) {
+            element.bind('click', goBack);
+            function goBack() {
+                history.back();
+                scope.$apply();
+            };
+        } //end link function
     }; //end return
   }]);
